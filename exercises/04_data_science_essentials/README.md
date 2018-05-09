422d26b90fee687677ff28d8dbb43600527af899
tathagatadg <tathagatadg@gmail.com>
2018-05-09 09:29:50 -0400

    changes to avoid spacy installation failures

--- exercises/04_data_science_essentials/README.md
+++ exercises/04_data_science_essentials/README.md
@@ -2,8 +2,8 @@ For this lab we will be using the Jupyter Notebook.
 Build the Dockerfile and start the container.
 
 ```bash
-docker build -t recommend .
-docker run -p 8888:8888 -p 9000:9000 recommend
+docker build -t talk_recommender .
+docker run -p 8888:8888 -p 9000:9000 -v $(pwd):/app talk_recommender
 ```
 
 Take the url from the log and load up the jupyter notebook  `talk_recommender.ipynb` in your broweser.
--- exercises/04_data_science_essentials/model.py
+++ exercises/04_data_science_essentials/model.py
@@ -8,23 +8,21 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
 def prediction():
-    df=pd.read_csv('data/talks.csv')
-
-    vectorizer = TfidfVectorizer(ngram_range=(1, 2), stop_words="english")
-    vectorized_text = vectorizer.fit_transform(df['description'])
-    labels = df[df.year == 2017]['label']
-    count_labeled = len(df[df.year == 2017])
-    vectorized_text_labeled = vectorized_text[:count_labeled]
-    vectorized_text_predict = vectorized_text[count_labeled:]
-
-    X_train, X_test, y_train, y_test = train_test_split(vectorized_text_labeled, labels, test_size=.3)
-    classifier = LinearSVC()
-    classifier.fit(X_train, y_train)
+    # Exercise A: Load the data
     
-    y_pred = classifier.predict(X_test)
-    predicted_talks_vector = classifier.predict(vectorized_text_predict)
-    df_2018 = df[df.year==2018]
-    predicted_talks_indexes = predicted_talks_vector.nonzero()[0] + count_labeled
-    results = df_2018.loc[predicted_talks_indexes][['id', 'description', 'presenters', 'title', 'location']]
 
+    # Exercise B: Feature Extraction
+
+
+    # Exercise C: Split into Training and Testing Set
+
+
+    # Exercise D: Train the model
+
+
+    # Exercise E: Make Predictions
+
+
+    # change the empty data frame with your predictions data frame
+    results = pd.DataFrame({'foo':[]})
     return results.to_dict(orient='records')
